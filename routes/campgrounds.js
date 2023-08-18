7e72f6e970bf82b9e0610ba67d0048177ad362fb
rmatej22 <rmatej545@gmail.com>
2023-08-18 18:25:33 +0200

    implement error alert messages using flash

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -33,6 +33,11 @@ router.get(
     const campground = await Campground.findById(req.params.id).populate(
       "reviews"
     );
+    console.log(campground);
+    if (!campground) {
+      req.flash("error", "Cannot find that campground!");
+      return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/show", { campground });
   })
 );
@@ -56,6 +61,10 @@ router.get(
   "/:id/edit",
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+      req.flash("error", "Cannot find that campground!");
+      return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/edit", { campground });
   })
 );
