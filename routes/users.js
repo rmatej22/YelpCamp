75448486dd74b3c5aeba42ca4c24315ac1dc9578
rmatej22 <rmatej545@gmail.com>
2023-09-09 10:46:06 +0200

    create form template and post route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const User = require("../models/user");
 
 router.get("/register", (req, res) => {
-  res.render('users/register');
+  res.render("users/register");
+});
+
+router.post("/register", async (req, res) => {
+  res.send(req.body);
 });
 
 module.exports = router;
