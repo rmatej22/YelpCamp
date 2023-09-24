f46f86ea2453492898cec44bf5582a4dd4f6ec22
rmatej22 <rmatej545@gmail.com>
2023-09-24 18:56:41 +0200

    protect delete review route by implementing isReviewAuthor middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,7 @@ const router = express.Router({ mergeParams: true });
 const catchAsync = require("../utils/catchAsync");
 const Review = require("../models/review");
 const Campground = require("../models/campground");
-const { validateReview, isLoggedIn } = require("../middleware");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 
 router.post(
   "/",
@@ -24,6 +24,7 @@ router.post(
 router.delete(
   "/:reviewId",
   isLoggedIn,
+  isReviewAuthor,
   catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
