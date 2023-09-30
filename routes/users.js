97a479b9fb48e814c7929b34609d5c8b79dec8fd
rmatej22 <rmatej545@gmail.com>
2023-09-30 15:17:44 +0200

    refactor and group users routes

--- routes/users.js
+++ routes/users.js
@@ -6,23 +6,24 @@ const passport = require("passport");
 const { storeReturnTo } = require("../middleware");
 const users = require("../controllers/users");
 
-router.get("/register", users.renderRegister);
+router
+  .route("/register")
+  .get(users.renderRegister)
+  .post(catchAsync(users.register));
 
-router.post("/register", catchAsync(users.register));
-
-router.get("/login", users.renderLogin);
-
-router.post(
-  "/login",
-  // use the storeReturnTo middleware to save the returnTo value from session to res.locals
-  storeReturnTo,
-  passport.authenticate("local", {
-    failureFlash: true,
-    failureRedirect: "/login",
-    keepSessionInfo: true,
-  }),
-  users.login
-);
+router
+  .route("/login")
+  .get(users.renderLogin)
+  .post(
+    // use the storeReturnTo middleware to save the returnTo value from session to res.locals
+    storeReturnTo,
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+      keepSessionInfo: true,
+    }),
+    users.login
+  );
 
 router.get("/logout", users.logout);
 
