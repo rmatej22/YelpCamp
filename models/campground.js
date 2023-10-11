61fcab315754ed1b72fa927c629e8106eb77dcfc
rmatej22 <rmatej545@gmail.com>
2023-10-11 18:48:39 +0200

    get geocoding info from the form and store it into database

--- models/campground.js
+++ models/campground.js
@@ -7,13 +7,24 @@ const ImageSchema = new Schema({
   filename: String,
 });
 
-ImageSchema.virtual('thumbnail').get(function(){
-  return this.url.replace('/upload', '/upload/w_200')
-})
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
 
 const CampgroundSchema = new Schema({
   title: String,
   images: [ImageSchema],
+  geometry: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
   price: Number,
   description: String,
   location: String,
