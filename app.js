fd38d0ba3937dc53ce762d826b2cc2a28d7f4024
rmatej22 <rmatej545@gmail.com>
2023-10-16 18:05:41 +0200

    update dbUrl

--- app.js
+++ app.js
@@ -21,10 +21,9 @@ const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 const mongoSanitize = require("express-mongo-sanitize");
 const MongoStore = require("connect-mongo");
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 
 // setup database connection
-// old connection --> "mongodb://localhost:27017/yelp-camp"
 mongoose.connect(dbUrl);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
